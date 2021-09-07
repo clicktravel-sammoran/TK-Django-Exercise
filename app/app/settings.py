561ce553e9e0f0916eade9bdd7b3bef545b83a83
clicktravel-sammoran <sam.moran@clicktravel.com>
2021-09-07 11:14:55 +0100

    Remove secret keys

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v(5m83g7gwh!111+(c)jb+#=*j*7mab2*d8-!2-p$gol!%@rq)'
+SECRET_KEY = "${DJANGO_SECRET_KEY}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
